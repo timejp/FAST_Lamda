6ec4e78c69284c3b1cd2e7aae68adf516933319b
timejp <tokijh@naver.com>
2017-02-20 16:01:15 +0900

    Lamda 기본 설정

--- app/src/main/java/com/timejh/lamda/MainActivity.java
+++ app/src/main/java/com/timejh/lamda/MainActivity.java
@@ -2,6 +2,8 @@ package com.timejh.lamda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("클릭");
+            }
+        });
     }
+
+    View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.button:
+                    String a = "aaa";
+                    break;
+            }
+        }
+    };
+
+    /* 하나의 함수 instance 로 작성
+    /* 함수를 객체화 한다
+    함수 이름 생략 / (파라미터 타입 생략) -> {
+        // 실행 블럭
+    }
+    */
 }
