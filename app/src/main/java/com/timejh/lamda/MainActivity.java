093df34398856926760380a3aaeb7ee2aaff2ce0
timejp <tokijh@naver.com>
2017-02-20 16:14:35 +0900

    Lamda 적용

--- app/src/main/java/com/timejh/lamda/MainActivity.java
+++ app/src/main/java/com/timejh/lamda/MainActivity.java
@@ -2,8 +2,11 @@ package com.timejh.lamda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,12 +16,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        EditText editText = (EditText) findViewById(R.id.editText);
+
+        // 람다 사용 조건
+        // 1. 콜백 객체가 인자로 넘어가는 곳
+        // 2. 콜백 객체에 함수가 하나여야 한다
+
+        button.setOnClickListener((v) -> {
                 System.out.println("클릭");
             }
-        });
+        );
+
+        editText.setOnClickListener((v) -> {
+
+            }
+        );
     }
 
     View.OnClickListener clickListener = new View.OnClickListener() {
